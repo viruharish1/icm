f0d1b96da48f62f54e8a655d56ef12cc934deb02
viruharish1 <viruharish1@gmail.com>
2017-05-24 20:31:38 +0530

    Add files via upload
    Complete changes in full code and working perfectly

--- Category.java
+++ Category.java
@@ -27,29 +27,24 @@ public class Category {
 	public void Login() throws InterruptedException
 	{
 
-		driver.get("http://220.225.128.97:9100");
-		
+		driver.get("http://220.225.128.97:9100");		
 		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 		
-		driver.switchTo().defaultContent();
-		
+		driver.switchTo().defaultContent();		
 		driver.manage().window().maximize();
 		
 		WebDriverWait wait1 = new WebDriverWait(driver, 60);
-		wait1.until(ExpectedConditions.elementToBeClickable(By.id("username")));
-		
+		wait1.until(ExpectedConditions.elementToBeClickable(By.id("username")));		
 		WebElement username = driver.findElement(By.id("username"));
 		username.sendKeys("admin");
 		
 		WebDriverWait wait2 = new WebDriverWait(driver, 60);
-		wait2.until(ExpectedConditions.elementToBeClickable(By.id("password")));
-		
+		wait2.until(ExpectedConditions.elementToBeClickable(By.id("password")));		
 		WebElement password = driver.findElement(By.id("password"));
 		password.sendKeys("admin");
 		
 		WebDriverWait wait3 = new WebDriverWait(driver, 60);
-		wait3.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/div/div/div[2]/div/div[3]/form/button")));
-		
+		wait3.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/div/div/div[2]/div/div[3]/form/button")));		
 		WebElement login = driver.findElement(By.xpath("/html/body/div[1]/div/div/div[2]/div/div[3]/form/button"));
 		login.click();		
 		
@@ -58,17 +53,15 @@ public class Category {
 	public void close()
 	{
 		WebDriverWait wait = new WebDriverWait(driver, 60);
-		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/div/div/form/div[1]/button")));
-		
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/div/div/form/div[1]/button")));		
 		WebElement close = driver.findElement(By.xpath("/html/body/div[1]/div/div/form/div[1]/button"));
-		close.click();  
+		close.click(); 
 	}
 	
 	public void saveclose()
 	{
 		WebDriverWait wait = new WebDriverWait(driver, 60);
-		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/div/div/form/div[3]/button[3]")));
-		
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/div/div/form/div[3]/button[3]")));		
 		WebElement saveclose = driver.findElement(By.xpath("/html/body/div[1]/div/div/form/div[3]/button[3]"));
 		saveclose.click();
 	}
@@ -76,21 +69,16 @@ public class Category {
 	@Test(priority=2,enabled=true)
 	public void company()
 	{
-		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-		
+		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);		
 		driver.switchTo().defaultContent();
-		
-		
-		
+						
 		WebDriverWait wait1 = new WebDriverWait(driver, 200);
-		wait1.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/nav/div/div[2]/ul/ul[1]/li")));
-		
+		wait1.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/nav/div/div[2]/ul/ul[1]/li")));		
 		WebElement company = driver.findElement(By.xpath("/html/body/div[1]/nav/div/div[2]/ul/ul[1]/li"));
 		company.click();
 		
 		WebElement company2 = driver.findElement(By.xpath("/html/body/div[1]/nav/div/div[2]/ul/ul[1]/li/ul/li[1]/a/span[2]"));
-		company2.click();
-	
+		company2.click();	
 		WebDriverWait wait2 = new WebDriverWait(driver, 200);
 		wait2.until(ExpectedConditions.elementToBeClickable(By.id("disableCreateButton")));
 		
@@ -98,26 +86,22 @@ public class Category {
 		addcompany.click();
 		
 		WebDriverWait wait3 = new WebDriverWait(driver, 200);
-		wait3.until(ExpectedConditions.elementToBeClickable(By.id("field_name")));
-		
+		wait3.until(ExpectedConditions.elementToBeClickable(By.id("field_name")));		
 		WebElement username = driver.findElement(By.id("field_name"));
 		username.sendKeys("testcom");
 		
 		WebDriverWait wait4 = new WebDriverWait(driver, 200);
-		wait4.until(ExpectedConditions.elementToBeClickable(By.id("field_street_address")));
-		
+		wait4.until(ExpectedConditions.elementToBeClickable(By.id("field_street_address")));		
 		WebElement street = driver.findElement(By.id("field_street_address"));
 		street.sendKeys("6th cross street");
 		
 		WebDriverWait wait5 = new WebDriverWait(driver, 200);
-		wait5.until(ExpectedConditions.elementToBeClickable(By.id("field_postalcode")));
-		
+		wait5.until(ExpectedConditions.elementToBeClickable(By.id("field_postalcode")));		
 		WebElement pcode = driver.findElement(By.id("field_postalcode"));
 		pcode.sendKeys("632602");
 		
 		WebDriverWait wait6 = new WebDriverWait(driver, 200);
-		wait6.until(ExpectedConditions.elementToBeClickable(By.id("field_registration_number")));
-		
+		wait6.until(ExpectedConditions.elementToBeClickable(By.id("field_registration_number")));		
 		WebElement regno = driver.findElement(By.id("field_registration_number"));
 		regno.sendKeys("2222");
 		
@@ -130,13 +114,11 @@ public class Category {
 	@Test(priority=3,enabled=true)
 	public void master()
 	{
-		driver.manage().timeouts().implicitlyWait(200, TimeUnit.SECONDS);
-		
+		driver.manage().timeouts().implicitlyWait(200, TimeUnit.SECONDS);		
 		driver.switchTo().defaultContent();
 		
 		WebDriverWait wait7 = new WebDriverWait(driver, 200);
-		wait7.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/nav/div/div[2]/ul/ul[3]/li")));
-		
+		wait7.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/nav/div/div[2]/ul/ul[3]/li")));		
 		WebElement master = driver.findElement(By.xpath("/html/body/div[1]/nav/div/div[2]/ul/ul[3]/li"));
 		master.click();
 	}
@@ -144,26 +126,22 @@ public class Category {
 	@Test(priority=4,enabled=true)
 	public void category()
 	{
-		driver.switchTo().defaultContent();
-		
+		driver.switchTo().defaultContent();		
 		WebElement category = driver.findElement(By.xpath("/html/body/div[1]/nav/div/div[2]/ul/ul[3]/li/ul/li[2]/a"));
 		category.click();
 		
 		WebDriverWait wait1 = new WebDriverWait(driver, 200);
-		wait1.until(ExpectedConditions.elementToBeClickable(By.id("disableCategoryCreateButton")));
-		
+		wait1.until(ExpectedConditions.elementToBeClickable(By.id("disableCategoryCreateButton")));		
 		WebElement addcategory = driver.findElement(By.id("disableCategoryCreateButton"));
 		addcategory.click();
 		
 		WebDriverWait wait2 = new WebDriverWait(driver, 200);
-		wait2.until(ExpectedConditions.elementToBeClickable(By.id("field_name")));
-		
+		wait2.until(ExpectedConditions.elementToBeClickable(By.id("field_name")));		
 		WebElement name = driver.findElement(By.id("field_name"));
 		name.sendKeys("qacat");
 		
 		WebDriverWait wait3 = new WebDriverWait(driver, 200);
-		wait3.until(ExpectedConditions.elementToBeClickable(By.id("field_description")));
-		
+		wait3.until(ExpectedConditions.elementToBeClickable(By.id("field_description")));		
 		WebElement description = driver.findElement(By.id("field_description"));
 		description.sendKeys("Qa category description");
 		
@@ -194,20 +172,17 @@ public class Category {
 		name.sendKeys("qa location");
 		
 		WebDriverWait wait2 = new WebDriverWait(driver, 200);
-		wait2.until(ExpectedConditions.elementToBeClickable(By.id("desc")));
-		
+		wait2.until(ExpectedConditions.elementToBeClickable(By.id("desc")));		
 		WebElement description = driver.findElement(By.id("desc"));
 		description.sendKeys("new qa location");
 		
 		WebDriverWait wait3 = new WebDriverWait(driver, 200);
-		wait3.until(ExpectedConditions.elementToBeClickable(By.id("field_longitude")));
-		
+		wait3.until(ExpectedConditions.elementToBeClickable(By.id("field_longitude")));		
 		WebElement longitude = driver.findElement(By.id("field_longitude"));
 		longitude.sendKeys("90.33");
 		
 		WebDriverWait wait4 = new WebDriverWait(driver, 200);
-		wait4.until(ExpectedConditions.elementToBeClickable(By.id("field_latitude")));
-		
+		wait4.until(ExpectedConditions.elementToBeClickable(By.id("field_latitude")));		
 		WebElement latitude = driver.findElement(By.id("field_latitude"));
 		latitude.sendKeys("80.33");
 		
@@ -226,8 +201,7 @@ public class Category {
 		category.click();
 		
 		WebDriverWait wait1 = new WebDriverWait(driver, 200);
-		wait1.until(ExpectedConditions.elementToBeClickable(By.id("searchQuery")));
-		
+		wait1.until(ExpectedConditions.elementToBeClickable(By.id("searchQuery")));		
 		WebElement catname = driver.findElement(By.id("searchQuery"));
 		catname.sendKeys("qacat");
 		
@@ -235,27 +209,23 @@ public class Category {
 		search.click();
 		
 		WebDriverWait wait2 = new WebDriverWait(driver, 200);
-		wait2.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/div/div/div[2]/table/tbody/tr/td[2]/div/div")));
-		
+		wait2.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/div/div/div[2]/table/tbody/tr/td[2]/div/div")));		
 		WebElement select = driver.findElement(By.xpath("/html/body/div[2]/div/div/div[2]/table/tbody/tr/td[2]/div/div"));
 		select.click();
 		
 		
 		WebDriverWait wait3 = new WebDriverWait(driver, 200);
-		wait3.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='disableAllCategoryElement']/div/ul/li[5]")));
-		
+		wait3.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='disableAllCategoryElement']/div/ul/li[5]")));		
 		WebElement selectloc = driver.findElement(By.xpath("//*[@id='disableAllCategoryElement']/div/ul/li[5]"));
 		selectloc.click();
 		
 		WebDriverWait wait4 = new WebDriverWait(driver, 200);
-		wait4.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='location']/div[1]/div/div/div/button")));
-		
+		wait4.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='location']/div[1]/div/div/div/button")));		
 		WebElement addloc = driver.findElement(By.xpath("//*[@id='location']/div[1]/div/div/div/button"));
 		addloc.click();
 		
 		WebDriverWait wait5 = new WebDriverWait(driver, 200);
-		wait5.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='disableAllCategoryLocationElement']/div[2]/typeahead/div/ul[1]/li/input")));
-		
+		wait5.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='disableAllCategoryLocationElement']/div[2]/typeahead/div/ul[1]/li/input")));		
 		WebElement addlocation = driver.findElement(By.xpath("//*[@id='disableAllCategoryLocationElement']/div[2]/typeahead/div/ul[1]/li/input"));
 		addlocation.sendKeys("qa location");
 		
@@ -267,10 +237,9 @@ public class Category {
 		WebDriverWait wait6 = new WebDriverWait(driver, 200);
 		wait6.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/div/div/form/div[1]/button"))).isDisplayed();
 		
-		Thread.sleep(2000); 		
-		
+		Thread.sleep(2000); 				
 		close();
-		
+
 	}
 	
 	@Test(priority=7,enabled=true)
